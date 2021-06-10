6886d6fe01e65cd412df90c6fb34f814b5dcf92b
SeraphinBoccacci <seraphinboccacci@gmail.com>
2021-06-10 17:08:09 +0200

    add coverage in pipeline

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,19 @@ module.exports = {
   testEnvironment: "node",
   testTimeout: 30000,
   rootDir: "./src",
+  coverageThreshold: {
+    global: {
+      // TO-DO increase this threshold by the time
+      branches: 50,
+      functions: 45,
+      lines: 60,
+      // statements: -10,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/config/",
+    "/models/",
+    "/types/",
+  ],
 };
